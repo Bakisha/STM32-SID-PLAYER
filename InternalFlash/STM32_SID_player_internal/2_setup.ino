96b9e224274f019982fa178d5fe0d77accc10b2f
Bakisha <bakisha@gmail.com>
2019-11-14 20:01:55 +0100

    Add files via upload

---
+++ InternalFlash/STM32_SID_player_internal/2_setup.ino
@@ -0,0 +1,147 @@
+//this could be in setup{}
+// but for now, i don't won't to make more mess then it is in main file
+
+inline void Init6502() {
+
+  if ( (RAM_SIZE ) < (SID_end ) ) {
+
+    LOW_RAM = true;
+    // hack to enable playing sids by relocating it's init/play address to lower MPU RAM address,(Mainly for STM32F103C)
+    // in RAM, player is at 0x300, .sid file data is at 0x400
+  }
+  else {
+    LOW_RAM = false;
+    // Lets see how much can i add before it crashed
+  }
+  //
+  // Blue6502 init
+  SID_play = SID_data[13] + (SID_data[12] * 256) ;// already defined in sid.h, but for future expansion (when changing subtunes, or maybe SD Card reading)
+  SID_init = SID_data[11] + (SID_data[10] * 256);
+  SID_speed = 20000; // for now 20000, (in uS) see how to calculate this number from sid header
+
+
+  if (DEFAULT_SONG > 0) { // if it's 0, upon start, play song that is default in sid header, otherwise, play tune specified as DEFAULT_SONG in "tunes.h"
+    SID_current_tune = DEFAULT_SONG;
+  }
+  // copy player from ROM to RAM
+
+  for ( i = 0; i < 256; i++) { // copy player to 0x300
+    RAM[i + 0x0300] = MyROM[i] ; //
+  }
+  RAM[0x0304] = SID_current_tune - 1 ; // set song numbe (-1 offset since in sid header tunes are 1 indexed, in player it's 0 indexed)
+  RAM[0x0307] = SID_data[10]; // init song Hi
+  RAM[0x0306] = SID_data[11]; // init song Lo
+  RAM[0x0310] = SID_data[12]; // play song Hi
+  RAM[0x030f] = SID_data[13]; // play song Lo
+
+
+
+  // transfer sid to RAM
+
+  for (uint16_t i = 0; i < SID_size - 0x7e; i++) { // data start at $7e offset
+
+    if ( LOW_RAM == true) {
+      if (i < (RAM_SIZE - 0x0400) ) { //  hack to load only amount of bytes that can fit into RAM from sid file. Hopefully, player routine is the beginning of file, and tune data is in the end
+        RAM[0x400 + i] =  SID_data[i + 0x7e] ; // load .sid to RAM , relocated at 0x0400
+      }
+    }
+    if ( LOW_RAM == false)   {
+      RAM[SID_start + i] =  SID_data[i + 0x7e] ; // load .sid directly to RAM in it's exact address . // TODO: Size check
+    }
+
+  }
+
+
+  reset6502(); // reset is at 0x0300
+
+}
+
+
+// Hardware timers initalization
+inline void InitHardware() {
+
+  // BlueSID -  // init irq
+
+  noInterrupts();
+  /*
+    pinMode(PB13, OUTPUT); // test pin 1
+    pinMode(PB12, OUTPUT); // test pin 2
+    pinMode(PB14, INPUT_PULLDOWN); // sense pin (fake PHI2 test)
+
+
+    pinMode (PA8, PWM); //   main volume output pin. at 1uS timer1, there is 72 (<magic_number>) different values of "volume"
+
+
+    digitalWrite(PB12, HIGH);
+    digitalWrite(PB13, HIGH);
+    //pwmWrite(PA8, max_step / 2); // 50% pwm for now
+
+
+
+    // Timer1 Channel is on PA8 PWM Pin (i know, i looked at bluepill's pinout)
+    // Timer1.setMode(TIMER_CH1, TIMER_OUTPUTCOMPARE); // no need, it is already set with: pinMode(PA8, PWM);
+    Timer1.setPeriod(period); // every 1 microseconds there will be 72 values of pwm (because bluepill is running at 72Mhz). It can be only be bigger then 1, but smaller then <multiplier>.
+    // Timer1.setCompare(TIMER_CH1, 1);      //  pwm of timer - no need to set it now, IRQ will deal with it
+
+
+    // Timer2 for IRQ
+    Timer2.setPrescaleFactor(1);
+    Timer2.setMode(TIMER_CH2, TIMER_OUTPUTCOMPARE);
+    Timer2.setPeriod(multiplier); // every xx microseconds interrupt run (total overhead us around 3.4uS +  code runtime (rest is time left for 6502 emulator )
+    Timer2.setCompare(TIMER_CH2, 1);      // not needed to put any value, it's irq, it's triggering on overflow, not on compare
+    Timer2.attachInterrupt(TIMER_CH2, irq_handler); // i could use better name
+  */
+
+  /*
+    // PA8 PWM output
+    //
+    pinMode(PA8, OUTPUT);
+
+    analogWriteResolution(8); // 8bit resolution - 0-255
+    analogWriteFrequency(1000000 / period);
+    // Not as easy as maple core, but ok
+    // Using analog write to write values between 0 and 255
+    analogWrite(PA8, ((1 << 8) / 2)); // set as 50% PWM
+  */
+
+  /*
+      pinMode(PA8, OUTPUT);
+    pinMode(PB13, OUTPUT);
+    pinMode(PB12, OUTPUT);
+    analogWriteResolution(8); // 16bit resolution - 0-65535
+    analogWriteFrequency(1000000 / period);
+    // Not as easy as maple core, but ok
+    // Using analog write to write values between 0 and 65535
+    analogWrite(PA8, ((1 << 8) / 2)); // set as 50% PWM
+    pinMode(PB13, OUTPUT);
+    pinMode(PB12, OUTPUT);
+  */
+
+  pinMode(PA8, OUTPUT);
+  pinMode(PB13, OUTPUT);
+  pinMode(PB12, OUTPUT);
+  // setup pwm on pin PA8 (timer1,channel1) on <period> us
+  // HardwareTimer *PWM = new HardwareTimer(TIM1); // must be before setup{}
+  PWM->pause();
+  PWM->setMode(1, TIMER_OUTPUT_COMPARE_PWM1, PA8); // set mode for timer1, channel1 witch correspond to pin PA8 // current mode is set to PWM1. 1 when counter is between zero and CaptureCompare, and 0 when it's between CaptureCompare and overflow value
+  PWM->setPrescaleFactor(1);
+  PWM->setOverflow( period * magic_number, TICK_FORMAT); //
+  PWM->resume();
+
+  // setup irq every <muliplier> us
+
+  // Instantiate HardwareTimer object. Thanks to 'new' instanciation, HardwareTimer is not destructed when setup() function is finished.
+  HardwareTimer *IRQtimer = new HardwareTimer(TIM2); // Setup for timer2 (could be any timer, but most of stm32 boards have timers 1 and 2 )
+  IRQtimer->setMode(2, TIMER_OUTPUT_COMPARE);  // channel 2, TIMER_OUTPUT_COMPARE is not assotiated on any pin
+  IRQtimer->setOverflow(multiplier, MICROSEC_FORMAT); // irq will trigger every <multiplier> uS
+  IRQtimer->attachInterrupt(irq_handler); // i could use a mega-super-ultra better name
+  IRQtimer->resume();
+
+
+  // delay(500);
+  interrupts(); // let the show begin
+  // end of Blue6502 init
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+}
---
+++ InternalFlash/STM32_SID_player_internal/STM32_SID_player_internal.ino
@@ -0,0 +1,115 @@
+// choose your STM32 microcontroller from board menu (USART port and USB serial can be disabled to increase RAM available)
+// it's recommended to set optimatization on FASTEST -O3 (from tool menu of Arduino IDE). (for some reasons LTO optimatization and Newlib Standard  doesn't work)
+// Can play only single speed irq based sids (no digis, sry. emulator is instruction based,not cycle exact based, and can't reach 1MHz speed). But it is fast enough to do irq instructions untill next frame.
+#define FILTER_FREQUENCY 12500 // maximum is 18000 for FILTER_SENSITIVITY 1 , lower it if increasing FILTER_SENSITIVITY. You'll know it if it's very high (it will totaly ruin volume)
+#define FILTER_SENSITIVITY  4// Lower is better. Best is 1 . It will consume irq time (<multiplier> must be higher), but FILTER_FREQUENCY can be set much higher. For value 8, maximum FILTER_FREQUENCY is 8000. For value 16, it's 4000. For 4, it's 12500
+// play with these two values to determine what is "SID-like" sound
+// NOTE: for now, filters are linear in all frequencies range
+
+#define USE_FILTERS // uncomment for testing, irq is  faster in calculations 
+#define RAM_SIZE 0x4000 + 0x400  // Set the amount of RAM that can be available to emulator (leave at least 2000 bytes for locals) (depends of microcontroller's RAM)
+
+#define USE_CHANNEL_1 // uncomment, for testing (affect only volume, not the calculations)
+#define USE_CHANNEL_2 // (but it will have effect on filter output in case of multiple voices is filtered)
+#define USE_CHANNEL_3 // 
+
+#define TUNE_PLAY_TIME 360 // Can't implement songlenghts, manual values are needed (in seconds) // TODO: add buttons to change subtunes // TODO2: try to determine silence in output, and skip to next tune
+#include  "tunes.h" // pick one
+#include  "SID.h"  // too much of them, looks nicer when separated in it's own .h file
+#include  "6502.h"
+
+HardwareTimer *PWM = new HardwareTimer(TIM1); // need to set it up here, before setup{}
+
+const uint8_t period = 1;// period for timer1, for frequency and resolution of PWM in uS
+// can be changed, lowest is 1. Must be less or equal of variable "multiplier". Can be squeeky.
+// value of 1 represent number of cpu cycles in 1 uS. (cpu_speed * period) is PWM resolution .
+// Best to set as multiplier, then lower it untill squeeks are gone
+// If set 1, it could be used even without external low-pass filter
+
+const uint8_t multiplier = 32 ; // (byte) set this for interrupt speed in uS (in general, how much slower then real SID). Don't set too small, irq might overlap (music will play  1/2 slower)
+// needed for Timer2 (it also affect calculations in frequency multiplications per irq- it may affect tunes that uses Test-bit).
+// Ideally, this should be 1 (to cycle-exact emulate SID), but irq will need to respond and exit in next 500nS
+// Not with Bluepill, but for 2$ board, i'll make what i can
+// Minimum is 1 (as a number, don't set iit, aas iiiit wiiiiiiil beeeeeee sloooooooooooooow) (but it is fun to see how bluepill cycle emulate SID :-) )
+// Maximum is 248 , but that will greatly lower quality of high frequency sounds (Pulse voices might not even work, noise will be on lower frequencies, etc...) But please do try it :-D
+// Based on current code, optimal timing is:
+// 40 (+/-4) for normal (72Mhz)
+// You could say that SAMPLE_RATE=1000000/multiplier
+// Sound is not buffered, volume is calculated every <multiplier> uS
+
+
+void setup() {
+  Init6502(); // Emulator/player initialization (located in inside 6502.ino )
+  InitHardware(); // Setup timers and interrupts (located in hardware_.ino )
+
+} // 
+
+void loop() {// One 6502 instruction per loop
+  // Player routine
+  // pritty much barebone
+  PB12_HIGH;
+  exec6502(); // execute 1 instruction
+  //  while (STAD4XX == 1) { // was $d404,$d40b,$d412,$d418 accessed?
+  //  } // it can be left out if <multiplier> is small enough, so no more then 4 or 5 6502 instruction is executed between irq's
+  if (JSR1003 == 1) { // JSR1003 check
+    JSR1003 = 0;
+    while (VIC_irq_request == 0) { // wait untill irq say it's new frame
+    }
+    VIC_irq_request = 0;
+  }
+
+  PB12_LOW; // test point - function overhead
+
+} // loop
+
+
+
+// INFO:
+// Working with STM32 boards, add in board manager:
+// https://github.com/stm32duino/BoardManagerFiles/raw/master/STM32/package_stm_index.json
+
+//      - only music based on raster irq (play address must not be 0, will not work with custom music player inside .sid file)
+//      - multi-speed .sid files are not supported, ( emulation of 6502 is not fast enough to keep up ).
+//      - .sid load address must be at $0400 or greater
+//
+// STM32F103C8
+// This project is purely for my own entertainment , so WITHOUT ANY WARRANTY!
+// I had tried to make it as much as posible to be portable for other microcontrollers
+// My choise of STM32F103C8 is only because of it's price
+// in bluepill's RAM, player is at 0x300, .sid file data is at 0x400 (if microcontrollers RAM is less then end of memory address of sid file, otherwise file data is in it's original memory address)
+// tunes that work with it are in /music/f103 subfolder
+
+
+// STM32F401CC board:
+//
+// next cheapest microcontroller. Much more RAM, but not full 65536 bytes. Around $F400 is last address that can be used (tunes that have higher end address will  be relocated)
+// tunes that work with it are in /f401 subfolder. Ofcorse, if tune in /f103 subfolder works on f103 microcontoller, it will work on f401 microcontoller too
+
+
+
+
+// SCHEMATICS:
+//
+//
+//    .-----------------.
+//    |                 |
+//    | STM32FxxxXXxx   |
+//    .------------|----.
+//     |G         P|
+//     |N         A|
+//     |D         8--R1----|------C2---------> OUDIO OUT
+//     |                   C
+//     |                   1
+//     .-------------------|----------------- GND
+//                        GND
+//    R1 = 100-500 Ohm
+//    C1 = 100 nF
+//    C2 = 10 uF
+//
+// If <period> is 1 , AUDIO OUT can be connected to PA8 (no need for R1,C1 and maybe C2). I don't think 1Mhz sample rate will be in hearing range
+
+
+
+// TODO :
+// Read .sid files from SD Card and copy it to RAM if it's less then available RAM
+// For now every .sid file is converted to byte array in /music subfolder , and added as #include"name_of_the_file.h" in SID.h
