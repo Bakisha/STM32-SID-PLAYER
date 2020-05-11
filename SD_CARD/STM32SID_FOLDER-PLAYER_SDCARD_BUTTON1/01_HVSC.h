078bd4f9433a20119856455a63181313f5b98315
Bakisha <bakisha@gmail.com>
2020-05-11 19:26:12 +0200

    pointers
    Pointers go to ROM (flash memory), not to RAM. 
    Must compile with O0 for bluepill or size of code will be greater then 128KB

--- SD_CARD/STM32SID_FOLDER-PLAYER_SDCARD_BUTTON1/01_HVSC.h
+++ SD_CARD/STM32SID_FOLDER-PLAYER_SDCARD_BUTTON1/01_HVSC.h
@@ -2,7 +2,7 @@
 #ifndef NUMBER_OF_ALL_FOLDERS
 #define NUMBER_OF_ALL_FOLDERS 1746
 
-const char * HVSC_ALL [NUMBER_OF_ALL_FOLDERS + 1] = {
+const char * const HVSC_ALL [] = {
   "DEMOS/0-9/",
   "DEMOS/A-F/",
   "DEMOS/Commodore/",
