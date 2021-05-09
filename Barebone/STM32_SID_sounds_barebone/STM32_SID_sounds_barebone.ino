d1b9378f6d0f492a6df2c67af0eba20b3626d492
Bakisha <bakisha@gmail.com>
2021-05-09 15:55:54 +0200

    STM core V2.0.0 update

--- Barebone/STM32_SID_sounds_barebone/STM32_SID_sounds_barebone.ino
+++ Barebone/STM32_SID_sounds_barebone/STM32_SID_sounds_barebone.ino
@@ -6,7 +6,7 @@
 
 // core detector
 #ifdef USE_HAL_DRIVER // Official ST cores. Support for multiple line of MPU
-#define USE_STM32duino_CORE //  Set in preferences: https://github.com/stm32duino/BoardManagerFiles/raw/master/STM32/package_stm_index.json and search stm32 in board manager. Choose stm32 cores by ST Microelectronics. Select your CPU from tools menu)
+#define USE_STM32duino_CORE //  Set in preferences: https://github.com/stm32duino/BoardManagerFiles/raw/master/package_stmicroelectronics_index.json and search stm32 in board manager. Choose stm32 cores by ST Microelectronics. Select your CPU from tools menu)
 #else
 #define USE_ROGER_CORE //  Set in preferences: http://dan.drown.org/stm32duino/package_STM32duino_index.json and search stm32F1 in board manager. Choose STM32F1xx core (NOTE: Only STM32F1 works)
 #endif
